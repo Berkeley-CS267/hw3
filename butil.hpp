c3149598acb80f4778c2be6dd35495ad06acd273
Alex Reinking <alex_reinking@berkeley.edu>
2020-04-17 13:27:34 -0700

    apply clang-format

--- butil.hpp
+++ butil.hpp
@@ -3,14 +3,13 @@
 #include <upcxx/upcxx.hpp>
 
 namespace BUtil {
-  template <typename ...Args>
-  void print(std::string format, Args... args) {
+template <typename... Args> void print(std::string format, Args... args) {
     fflush(stdout);
     upcxx::barrier();
     if (upcxx::rank_me() == 0) {
-      printf(format.c_str(), args...);
+        printf(format.c_str(), args...);
     }
     fflush(stdout);
     upcxx::barrier();
-  }
 }
+} // namespace BUtil
