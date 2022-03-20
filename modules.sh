f32e9d3bf57f0128d5efadcea94d69997eb4aa7e
Vivek Bharadwaj <vivek_bharadwaj@berkeley.edu>
2022-03-20 00:33:09 +0000

    Changed modules.sh so that cmake can locate UPCXX.

--- modules.sh
+++ modules.sh
@@ -1,5 +1,5 @@
-module swap PrgEnv-intel PrgEnv-cray
+module swap PrgEnv-intel PrgEnv-gnu
 module unload craype-haswell
 module load craype-mic-knl
-module load upcxx-cs267
+module load upcxx
 module load cmake
